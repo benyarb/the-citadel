f98cad253e5218ac1c26f013361542fc85e8a56b
Ben Yarbrough <benyarb@gmail.com>
2023-04-26 10:52:20 -0400

    Meta title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,17 @@ import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`page px-12 ${inter.className}`}>
+      <Head>
+        <title>The Citadel by benyarb</title>
+      </Head>
+
       <Header />
 
       <main className="mx-auto py-12">
